32430ce825a0f834063af9ab3aa525d292f173ac
Geo69-ops <82852931+Geo69-ops@users.noreply.github.com>
2022-08-28 07:58:17 -0500

    Profile actualizado

--- src/main/java/com/hwe/helloworldenterprise/entity/Profile.java
+++ src/main/java/com/hwe/helloworldenterprise/entity/Profile.java
@@ -1,4 +1,37 @@
 package com.hwe.helloworldenterprise.entity;
 
+public class Profile {import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name= "Profile")
 public class Profile {
+    @Id
+    private long ID;
+    private String name;
+    private String email;
+    private String picture;
+    private String phone;
+    private Enum_role role;
+    private Date created_date;
+
+    public Profile(long ID, String name, String email, String picture, String phone, Enum_role role, Date created_date) {
+        this.ID = ID;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.phone = phone;
+        this.role = role;
+        this.created_date = created_date;
+    }
+}
 }
